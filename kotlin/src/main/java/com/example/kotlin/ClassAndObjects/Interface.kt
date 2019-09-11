16002a5030fbb7ca9733635ba31830eefbc6df1a
= <zshinemeng@icloud.com>
2019-09-11 16:40:48 +0800

    add interface and visibility modifiers file

--- kotlin/src/main/java/com/example/kotlin/ClassAndObjects/Interface.kt
+++ kotlin/src/main/java/com/example/kotlin/ClassAndObjects/Interface.kt
@@ -1,2 +1,137 @@
 package com.example.kotlin.ClassAndObjects
 
+interface MyInterface {
+    fun bar()
+    fun foo() {
+
+    }
+}
+
+
+/**
+ * Implement interface
+ */
+
+class Child : MyInterface {
+
+    override fun bar() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+
+/**
+ * Properties in Interface
+ */
+
+interface MyInterface2 {
+    val prop: Int
+    val propertyWithImplementation: String
+        get() = "foo"
+
+    fun foo() {
+        println(prop)
+    }
+}
+
+class Child2 : MyInterface2 {
+    override val prop: Int = 29
+}
+
+
+/**
+ * Interface Inheritance
+ */
+
+interface Named {
+    val name: String
+}
+
+interface PersonAnother : Named {
+    val firstname: String
+    val lastname: String
+
+    override val name: String
+        get() = "$firstname $lastname"
+}
+
+data class Employee(override val firstname: String, override val lastname: String) : PersonAnother {
+
+}
+
+
+/**
+ * Resolving overriding conflicts
+ */
+interface A1 {
+    fun foo() {
+        println("A1")
+    }
+
+    fun bar()
+}
+
+interface B1 {
+    fun foo() {
+        println("B1")
+    }
+
+    fun bar() {
+        println("bar")
+    }
+}
+
+class C1 : A1 {
+
+    override fun bar() {
+        println("bar")
+    }
+}
+
+class D : A1, B1 {
+    override fun foo() {
+        super<A1>.foo()
+        super<B1>.foo()
+    }
+
+    override fun bar() {
+        super<B1>.bar()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- kotlin/src/main/java/com/example/kotlin/ClassAndObjects/VisibilityModifiers.kt
+++ kotlin/src/main/java/com/example/kotlin/ClassAndObjects/VisibilityModifiers.kt
@@ -1,2 +1,38 @@
 package com.example.kotlin.ClassAndObjects
 
+class Packages {
+    // visible inside VisibilityModifiers.kt
+    private fun foo() {
+
+    }
+
+    // property is visible everywhere
+    // setter is visible only in VisibilityModifiers.kt
+    var bar: Int = 5
+        private set
+
+    // visible inside the same module
+    internal val baz = 6
+}
+
+// ------->  ClassAndInterface  <----------
+open class Outer {
+    private val a = 1
+    protected open val b = 2
+    internal val c = 3
+    val d = 4
+
+    protected class Nested {
+        val e: Int = 5
+    }
+
+    //todo kotlin progress here
+}
+
+
+
+
+
+
+
+
